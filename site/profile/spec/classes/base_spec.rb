6bf64dbed6ee320fe4506bfb186f2d43e3a3751e
David Gwilliam <david.gwilliam@slalom.com>
2016-06-08 14:22:09 -0700

    subject > it

--- site/profile/spec/classes/base_spec.rb
+++ site/profile/spec/classes/base_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'profile::base' do
   it 'should include class ntp' do
-    expect(it).to contain_class('ntp')
-    expect(it).to contain_class('failure::guaranteed')
+    expect(subject).to contain_class('ntp')
+    expect(subject).to contain_class('failure::guaranteed')
   end
 end
